4518b9589bdd607f511d56ab063a71411c6c148c
Conslatekoyo <conslatekoyo@gmail.com>
2022-09-15 23:43:53 +0300

    added views

--- wallet/urls.py
+++ wallet/urls.py
@@ -10,6 +10,7 @@ from .views import register_transaction
 from .views import register_card
 from .views import register_thirdparty
 from .views import register_reward
+from . import views
 
 
 urlpatterns=[
@@ -23,6 +24,29 @@ urlpatterns=[
     path("transaction/",register_transaction,name="transactions"),
     path("card/",register_card,name="cards"),
     path("thirdparty/",register_thirdparty,name="thirdparty"),
-    path("reward/",register_reward,name="reward")
+    path("reward/",register_reward,name="reward"),
+    path("customers/",views.list_customers, name="list"),
+    path("accounts/",views.list_account, name="acc"),
+    path("cards/",views.list_card, name="car"),
+    path("currencies/",views.list_currency, name="curr"),
+    path("loans/",views.list_loan, name="loa"),
+    path("notifications/",views.list_notification, name="notifi"),
+    path("receipts/",views.list_receipt, name="rece"),
+    path("rewards/",views.list_reward, name="rew"),
+    path("thirdparties/",views.list_thirdparty, name="third"),
+    path("transactions/",views.list_transaction, name="transact"),
+    path("wallets/",views.list_wallet, name="wallet")
+
+
+
+
+
+
+
+
+
+
+
+
 
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -1,6 +1,10 @@
 from locale import currency
+from urllib import request
 from django.shortcuts import render
 import wallet
+from wallet import forms
+from .models import Account, Card, Currency, Customer, Loan, Notification, Receipt, Reward, ThirdParty, Transaction, Wallet
+from . import forms
 
 from wallet.forms import CurrencyRegistration, CustomerRegistrationForm, LoanRegistration
 from wallet.forms import WalletRegistration
@@ -19,10 +23,16 @@ from wallet.forms import RewardRegistration
 
 # Create your views here.
 def register_customer(request):
+  if request.method=="POST":
+    form=CustomerRegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+  else:
     form=CustomerRegistrationForm()
-    return render(request,"wallet/customer.html",  
-    {"cus":form}
-      )
+  return render(request,"wallet/customer.html",{"customer":form})
+  # form=CustomerRegistrationForm()
+
+    
 def register_wallet(request):
   pochi=WalletRegistration()
   return render(request,"wallet/wallet.html",
@@ -78,4 +88,48 @@ def register_reward(request):
   reward=RewardRegistration()
   return render(request,"wallet/reward.html",
   {"rewards":reward}
-  )   
+  ) 
+
+def list_customers(request):
+  customer=Customer.objects.all()
+  return render(request,"wallet/customer_list.html",{"Customers":customer})
+
+def list_account(request):
+  account= Account.objects.all()
+  return render(request,"wallet/account_list.html",{"Accounts":account})
+
+def list_card(request):
+  card=Card.objects.all()
+  return render(request,"wallet/card_list.html",{"Cards":card})
+
+def list_currency(request):
+  currency=Currency.objects.all()
+  return render(request,"wallet/currency_list.html",{"Currencies":currency})
+
+def list_loan(request):
+  loan=Loan.objects.all()
+  return render(request,"wallet/loan_list.html",{"Loans":loan})
+
+def list_notification(request):
+  notification=Notification.objects.all()
+  return render(request,"wallet/notification_list.html",{"Notifications":notification})
+
+def list_receipt(request):
+  receipt=Receipt.objects.all()
+  return render(request,"wallet/receipt_list.html",{"Receipts":receipt})
+
+def list_reward(request):
+  reward=Reward.objects.all()
+  return render(request,"wallet/reward_list.html",{"Rewards":reward})
+
+def list_thirdparty(request):
+  thirdparty=ThirdParty.objects.all()
+  return render(request,"wallet/thirdparty_list.html",{"Thirdparties":thirdparty})
+
+def list_transaction(request):
+  transaction=Transaction.objects.all()
+  return render(request,"wallet/transaction_list.html",{"Transactions":transaction})
+
+def list_wallet(request):
+  wallet=Wallet.objects.all()
+  return render(request,"wallet/wallet_list.html",{"Wallets":wallet})
